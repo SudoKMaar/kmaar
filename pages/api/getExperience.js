9b2fbab20b159a4c75d0a6eee34724f8a0c0b192
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-08-14 18:41:20 +0530

    hopefully everything is fine

--- pages/api/getExperience.js
+++ pages/api/getExperience.js
@@ -2,8 +2,9 @@ import { groq } from "next-sanity";
 import { client } from "../../sanity.config";
 
 const query = groq`*[_type == "experience"]`;
+const cleanedQuery = query.replace(/\s/g, "");
 
 export default async function experienceAPI(req, res) {
-  const experience = await client.fetch(query);
+  const experience = await client.fetch(cleanedQuery);
   res.status(200).json({ experience });
 }
