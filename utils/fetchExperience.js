fe267c4553b2d8cebee9847bcbadcd3308384ded
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-08-14 18:59:47 +0530

    hopefully everything is fine

--- utils/fetchExperience.js
+++ utils/fetchExperience.js
@@ -1,6 +1,5 @@
 export const fetchExperience = async () => {
-  const res = await fetch();
-  // `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hello`);
   const data = await res.json();
   const experience = data.experience;
   return experience;
