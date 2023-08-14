16f909bbb2dd4fef9caebad286f0a64c605c4902
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-08-14 18:11:08 +0530

    hopefully everything is fine

--- pages/api/getPageInfo.js
+++ pages/api/getPageInfo.js
@@ -1,7 +1,7 @@
 import { groq } from "next-sanity";
 import { client } from "../../sanity.config";
 
-const query = groq`*[_type == "pageInfo"]`;
+const query = groq`*[_type == "pageInfo"][0]`;
 
 export default async function pageInfoAPI(req, res) {
   const pageInfo = await client.fetch(query);
