5dcd9505a5a8c99b1944fd24ed3a2132727a9e98
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-08-14 19:02:39 +0530

    hopefully everything is fine

--- utils/fetchProjects.js
+++ utils/fetchProjects.js
@@ -1,7 +1,5 @@
 export const fetchProjects = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hello`);
   const data = await res.json();
   const projects = data.projects;
   return projects;
--- utils/fetchSkills.js
+++ utils/fetchSkills.js
@@ -1,5 +1,5 @@
 export const fetchSkils = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hello`);
   const data = await res.json();
   const skills = data.skills;
   return skills;
