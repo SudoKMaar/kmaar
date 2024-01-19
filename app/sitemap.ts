34258cb90eb8d25d0546c0b39f9ceff4852ae69e
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2024-01-19 21:26:32 +0530

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,71 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
       changeFrequency: "hourly",
     },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+      lastModified: new Date(),
+      priority: 0.5,
+      changeFrequency: "hourly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/codepen`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/discord`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/facebook`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/github`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/instagram`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/linkedin`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/reddit`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/resume`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/snapchat`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/twitter`,
+      lastModified: new Date(),
+      priority: 0.8,
+      changeFrequency: "monthly",
+    },
   ];
 }
